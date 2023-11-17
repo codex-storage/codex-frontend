8b3b7adad3e39557e90ee2e5a1ca8cb4492f7cfd
Shaun Orssaud <72015533+Shorssaud@users.noreply.github.com>
2023-11-17 15:22:43 +0900

    UPD api endpoints for upload and download

--- frontend/src/pages/data/tabs/download/DownloadTab.tsx
+++ frontend/src/pages/data/tabs/download/DownloadTab.tsx
@@ -12,7 +12,7 @@ function DownloadTab() {
     console.log(filename);
     console.log(cid);
     fetch(
-      `/api/codex/v1/content/${cid}`,
+      `/api/codex/v1/data/${cid}`,
       {
         headers:
           (nodeInfo.auth !== null && {
