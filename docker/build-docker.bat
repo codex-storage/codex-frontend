db2ece4b58d15d259f53afd0a2dc52d859d1c0b4
Veaceslav Doina <20563034+veaceslavdoina@users.noreply.github.com>
2023-12-01 10:09:20 +0200

    Fix Nginx template loading (#10)

--- docker/build-docker.bat
+++ docker/build-docker.bat
@@ -1,8 +1,9 @@
 :: Variables
-set repository=thatbenbierens/codex-frontend
-set tag=initial
-set app_src=../frontend
+set REPOSITORY=thatbenbierens/codex-frontend
+set TAG=initial
+set APP_SRC=../frontend
+set NGINX_TEMPLATE=docker/nginx.template
 
 :: Build
-docker build --build-arg APP_SRC=%app_src% -t %repository%:%tag% .
-docker push %repository%:%tag%
+docker build --build-arg APP_SRC=%APP_SRC% --build-arg NGINX_TEMPLATE=%NGINX_TEMPLATE% -t %REPOSITORY%:%TAG% .
+docker push %REPOSITORY%:%TAG%
